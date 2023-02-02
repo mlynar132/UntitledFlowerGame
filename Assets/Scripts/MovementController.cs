0676d73fd5f96f70c6d29fe7753078219b0b87bd
sanyaogunjobi7 <115067086+sanyaogunjobi7@users.noreply.github.com>
2023-02-02 04:46:20 +0100

    Player Movement with RB
    Movement with Rigidbody

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class MovementController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float moveSpeed;
+    private Rigidbody rb;
+    private bool jumpKeyWasPressed;
+    public float jumpForce;
+    private float horizontalInput;
+    private bool isGrounded;
+
+    [SerializeField] public Transform GroundCheckTransform;
+    [SerializeField] private LayerMask playerMask;
+
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
+    }
+
+   
+    void Update()
+    {
+
+        
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        { 
+                jumpKeyWasPressed = true;
+        
+          
+        }
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (Physics.OverlapSphere(GroundCheckTransform.position, 0.1f, playerMask).Length== 1)
+        {
+            return;
+        }
+
+        if (jumpKeyWasPressed)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            jumpKeyWasPressed = false;
+        }
+
+        rb.velocity = new Vector3(horizontalInput * moveSpeed, rb.velocity.y, 0f);
     }
+
+   
 }
+
