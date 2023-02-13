704e8db93d7576d6f13a0cf95ec3877333e05bda
sanyaogunjobi7 <115067086+sanyaogunjobi7@users.noreply.github.com>
2023-02-13 09:59:02 +0100

    Main menu, volume, brightness controls
    Main menu, volume, brightness controls

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,14 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
 public class MainMenu : MonoBehaviour
 {
+
+    public Slider slider;
+    public float sliderValue;
+    public Image panelBrightness;
+
+
+    public AudioMixer audioMixer;
     
     public void StartGame()
     {
 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        slider.value = PlayerPrefs.GetFloat("brightness", 0.5f);
+        panelBrightness.color = new Color(panelBrightness.color.r, panelBrightness.color.g, panelBrightness.color.b, slider.value);
+    }
+
+
+    private void Update()
+    {
+   
+
+    }
+
+    public void changeSlider(float value)
+    {
+
+        sliderValue = value;
+
+        PlayerPrefs.SetFloat("brightness", sliderValue);
+        panelBrightness.color = new Color(panelBrightness.color.r, panelBrightness.color.g, panelBrightness.color.b, slider.value);
     }
 
     public void QuitGame()
@@ -17,13 +43,23 @@ public class MainMenu : MonoBehaviour
 
         Debug.Log("Game End!!!");
         Application.Quit();
-        
+
+    }
+
+    public void SetFullScreen( bool isFullScreen)
+    {
+
+        Screen.fullScreen = isFullScreen;
 
     }
 
 
+    public void SetVolume(float volume)
+    {
 
+        audioMixer.SetFloat("volume", volume);
 
+    }
 
 
 
